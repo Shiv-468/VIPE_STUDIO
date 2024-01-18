ef16c1da795e926e84406c6beb287215436c2d76
Shiv-468 <shivmuwal.147@gmail.com>
2024-01-18 16:45:57 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'gray': ('0px 20px 60.571px 0px #E5E9F6'),
+        'purple':('0px 20px 50px 0px rgba(39, 0, 124, 0.50)'),
+      },
+      fontFamily:{
+        'Jost': ('"Jost", sans-serif'),
+        'Inter': ('Inter'),
+        'Konexy': ['"Konexy Personal Use", sans-serif'],
+        'Gilroy-Light': ('Gilroy-Light'),
+        'Gilroy-Bold': ('Gilroy-Bold'),
+      }
+    },
+  },
+  plugins: [],
+}
+
